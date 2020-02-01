e846998a1f9fb43e7c2a20871c17660921573d40
Cassidy Jensen <schnauzersal@LAPTOP-KU897ACP.localdomain>
2020-02-01 14:33:30 -0700

    Text glows upon hover

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-function lower(x) {
-    x.style.bottom = "-50px";
-  }
+
+console.log("Testing");
+
+// document.getElementById("mountain").addEventListener("lower", mouseIn);
+
+// function mouseOver() {
+//   document.getElementById("demo").style.color = "red";
+// }
+
+// function lower{
+//     document.getElementById("mountain");
+//     x.style.bottom = "-50px";
+//   }
   
-  function normal(x) {
-    x.style.bottom = "0px";
-  }
+//   function normal(x) {
+//     x.style.bottom = "0px";
+//   }
