9d5a5b18dcb74fab666cefe12d1d0af0b657548a
Cassidy Jensen <schnauzersal@LAPTOP-KU897ACP.localdomain>
2020-02-01 15:02:25 -0700

    One star repeatedly blinks in and out

--- js/main.js
+++ js/main.js
@@ -1,5 +1,59 @@
 
-console.log("Testing");
+window.onload = () => {
+    startSetTimeoutAnimation();
+  };
+  
+  function startSetTimeoutAnimation() {
+    console.log("Testing");
+
+    const refreshRate = 1000 / 20;
+    const maxOpacity = 1;
+    let stars = document.getElementById('star1');
+    let fade = .1;
+    let opac = 0;
+  
+    window.setInterval(() => {
+      opac = opac + fade;
+      if (opac > maxOpacity || opac < 0) {
+        fade = fade * (-1);
+      }
+      stars.style.opacity = opac;
+    }, refreshRate);
+  }
+
+//   function startAnimFrameAnimation() {
+//     const refreshRate = 1000 / 60;
+//     const maxXPosition = 400;
+//     let rect = document.getElementById('rect1');
+//     let speedX = 1;
+//     let positionX = 0;
+  
+//     function step() {
+//       positionX = positionX + speedX;
+//       if (positionX > maxXPosition || positionX < 0) {
+//         speedX = speedX * (-1);
+//       }
+//       rect.style.left = positionX + 'px';
+//       window.requestAnimationFrame(step);
+//     }
+  
+//     window.requestAnimationFrame(step);
+//   }
+  
+
+
+// var requestAnimationFrame = window.requestAnimationFrame;
+
+// var stars = document.getElementByClass("smallStar");
+// var currentOpacity = 1;
+
+// function animateStars(){
+//     currentOpacity -= .1;
+//     stars.style.opacity = currentOpacity;
+
+//     requestAnimationFrame(animateStars);
+// }
+// animateStars();
 
 // document.getElementById("mountain").addEventListener("lower", mouseIn);
 
